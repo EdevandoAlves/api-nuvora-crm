b935a1cf5666caa0be52e9e83996eede009fe2b2
Edevando Alves <edevando.xavier@gmail.com>
2026-08-24 13:12:01 -0300

    feat: pong router

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,12 @@
 import { Module } from "@nestjs/common";
 import { UsersModule } from "./users/users.module";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
-  imports: [UsersModule],
+  controllers: [AppController],
+  providers: [AppService],
+  imports: [UsersModule, ConfigModule.forRoot()],
 })
-export class AppModule { }
+export class AppModule {}
